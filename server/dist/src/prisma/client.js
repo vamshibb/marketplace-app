620499e2892ae0a5c57534e7507de794375b1270
vamshi bandela <bandelav00@gmail.com>
2026-06-03 10:31:42 -0400

    fix production build and prisma deployment

--- server/dist/src/prisma/client.js
+++ server/dist/src/prisma/client.js
@@ -2,7 +2,7 @@
 Object.defineProperty(exports, "__esModule", { value: true });
 exports.prisma = void 0;
 require("dotenv/config");
-const client_1 = require("@prisma/client");
+const client_1 = require("../generated/prisma/client");
 const adapter_pg_1 = require("@prisma/adapter-pg");
 exports.prisma = new client_1.PrismaClient({
     adapter: new adapter_pg_1.PrismaPg({
--- server/src/prisma/client.ts
+++ server/src/prisma/client.ts
@@ -1,5 +1,5 @@
 import "dotenv/config";
-import { PrismaClient } from "@prisma/client";
+import { PrismaClient } from "../generated/prisma/client";
 import { PrismaPg } from "@prisma/adapter-pg";
 
 export const prisma = new PrismaClient({
