c0e83fafbdbfef4ec18a118b4133b68b09e0c29c
vamshi bandela <bandelav00@gmail.com>
2026-08-19 22:41:49 +0530

    refactor(apiResponse): enhance ApiResponse interface and return type

--- server/src/utils/apiResponse.ts
+++ server/src/utils/apiResponse.ts
@@ -1,7 +1,13 @@
+export interface ApiResponse<T> {
+  success: true;
+  message?: string;
+  data: T;
+}
+
 export const successResponse = <T>(
   data: T,
   message?: string
-) => ({
+): ApiResponse<T> => ({
   success: true,
   message,
   data,
