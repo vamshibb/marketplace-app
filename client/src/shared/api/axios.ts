39208b912e0e4f1e7a54628597c2d5a29346ae2e
vamshi bandela <bandelav00@gmail.com>
2026-09-01 13:17:46 +0530

    feat(auth): attach jwt to axios requests

--- client/src/shared/api/axios.ts
+++ client/src/shared/api/axios.ts
@@ -1,10 +1,21 @@
 import axios from "axios";
 
 import { env } from "../../app/config/env";
+import { useAuthStore } from "../../features/auth/hooks/useAuthStore";
 
 export const api = axios.create({
   baseURL: env.apiBaseUrl,
   headers: {
     "Content-Type": "application/json",
   },
+});
+
+api.interceptors.request.use((config) => {
+  const token = useAuthStore.getState().token;
+
+  if (token) {
+    config.headers.Authorization = `Bearer ${token}`;
+  }
+
+  return config;
 });
